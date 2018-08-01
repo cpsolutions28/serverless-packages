b73af03e1ebb4fae3e80bfcf0dce2371b7c11cd6
p766682 <eddie.lewis@nab.com.au>
2018-08-02 09:43:39 +1000

    add the env settings file

--- dotenv.js
+++ dotenv.js
@@ -1,6 +1,6 @@
-const dotenv = require('dotenv');
+const dotenv = require('dotenv'); // load environment variables from a .env file - should only contain env specific vars (dev, prod, etc) so not encouraged to commit
 const json = require('format-json');
-const _ = require('lodash');
+const _ = require('lodash'); // utility functions for common tasks - Arrays, Collections, Date, Lang, NUmber, String, etc
 const sharedConfig = dotenv.config({path: '../../.env'});
 const localConfig = dotenv.config();
 
